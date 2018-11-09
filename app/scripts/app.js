b7e7824245d142849eb073cdf89844781cd0d2a5
WYCodeBook <wyu10amy@hotmail.com>
2018-11-09 01:03:13 -0800

    new

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,5 +8,35 @@
  *
  * Main module of the application.
  */
-angular
-  .module('routingQuizApp', []);
+ angular
+   .module('routingQuizApp', ['ui.router'])
+   .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+     $urlRouterProvider.otherwise('/');
+
+     $stateProvider
+       .state('home', {
+         url: '/',
+         templateUrl: 'views/instructions.html'
+       })
+       .state('brick', {
+         url: '/bricks/:color',
+         templateUrl: 'views/bricks.html',
+         controllerProvider: function($stateParams) {
+           var color = $stateParams.color;
+           color = color[0].toUpperCase() + color.slice(1);
+           var ctrlName = color + 'BricksCtrl';
+           return ctrlName;
+         },
+         controllerAs: 'brick'
+       })
+       .state('brick.cart', {
+         url: '/cart',
+         templateUrl: 'views/cart.html',
+         controller: 'CartCtrl as cart'
+       })
+       .state('cart', {
+         url: '/cart',
+         templateUrl: 'views/cart.html',
+         controller: 'CartCtrl as cart'
+       });
+   }]);
